9f1428d273304ce184d39b1146f9c550a1330f9c
liqian0505 <liqian_05@163.com>
2019-07-05 17:19:22 +0800

    add files

---
+++ src/test/java/org/springframework/accessingdatamongodb/AccessingDataMongodbApplicationTests.java
@@ -0,0 +1,16 @@
+package org.springframework.accessingdatamongodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccessingDataMongodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
